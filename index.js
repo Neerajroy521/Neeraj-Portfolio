de34337d63a7c1e18f01fea3527954075f56c1bd
NeerajSingh521 <151426558+Neerajroy521@users.noreply.github.com>
2025-04-02 15:07:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// spinnering wrapper loading
+
+const spinnerWrapper = document.querySelector('.spinner-wrapper');
+
+window.addEventListener('load', () => {
+    spinnerWrapper.style.opacity = '0';
+
+})
+
+setTimeout(() => {
+    spinnerWrapper.style.display = 'none';
+}, 200)
+
